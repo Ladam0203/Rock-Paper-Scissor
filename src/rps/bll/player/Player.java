63d221d6dfe9fdb7d9fd3c7ea118b3f6800d862b
Ladam0203 <adam7605@easv365.dk>
2022-03-02 12:00:44 +0100

    Update Player.java

--- src/rps/bll/player/Player.java
+++ src/rps/bll/player/Player.java
@@ -46,70 +46,81 @@ public class Player implements IPlayer {
      * @param state Contains the current game state including historic moves/results
      * @return Next move
      */
+    private double repeatLast = 0;
+    private double counterLast = 0;
+    private double surrenderLast = 0;
     @Override
     public Move doMove(IGameState state) {
         //Historic data to analyze and decide next move...
         ArrayList<Result> results = (ArrayList<Result>) state.getHistoricResults();
 
-        int learnSize = 6;
-        List<Result> recent = results.subList(Math.max(results.size() - learnSize, 0), results.size());
-        double repeatLast = 0;
-        double counterLast = 0;
-        double surrenderLast = 0;
-        for (int i = 1; i < recent.size(); i++) {
-            if (recent.get(i-1).getWinnerMove() == recent.get(i).getWinnerMove())
-            {
-                repeatLast++;
-            }
-            else if ((recent.get(i-1).getWinnerMove() == Move.Rock && recent.get(i).getWinnerMove() == Move.Scissor) ||
-                    (recent.get(i-1).getWinnerMove() == Move.Scissor && recent.get(i).getWinnerMove() == Move.Paper) ||
-                    (recent.get(i-1).getWinnerMove() == Move.Paper && recent.get(i).getWinnerMove() == Move.Rock))
-            {
-                counterLast++;
-            }
-            else
-            {
-                surrenderLast++;
-            }
-        }
-        System.out.println(repeatLast + " " + counterLast + " " + surrenderLast);
+        int depth = 5;
+        List<Result> recent = results.subList(Math.max(results.size() - depth, 0), results.size());
 
-        if (recent.size() < 1)
+        if (recent.size() >= 1)
         {
-            return Move.Scissor;
+            for (int i = recent.size()-1; i > 1; i--) {
+                Result earlier = recent.get(recent.size()-1);
+                Result latter = recent.get(recent.size()-2);
+                if (latter.getLoserPlayer() == this) {
+                    if (earlier.getWinnerMove() == latter.getWinnerMove() && latter.getType() != ResultType.Tie) {
+                        counterLast = (counterLast+1)/2;
+                        repeatLast = (counterLast+0)/2;
+                        surrenderLast = (counterLast+0)/2;
+                    }
+                    else if (earlier.getWinnerMove() == Move.Rock && latter.getWinnerMove() == Move.Scissor ||
+                            earlier.getWinnerMove() == Move.Scissor && latter.getWinnerMove() == Move.Paper ||
+                            earlier.getWinnerMove() == Move.Paper && latter.getWinnerMove() == Move.Rock)
+                    {
+                        counterLast = (counterLast+0)/2;
+                        repeatLast = (counterLast+1)/2;
+                        surrenderLast = (counterLast+0)/2;
+                    }
+                    else {
+                        counterLast = (counterLast+0)/2;
+                        repeatLast = (counterLast+0)/2;
+                        surrenderLast = (counterLast+1)/2;
+                    }
+                }
+            }
         }
         else
         {
-            Move repeatMove = recent.get(recent.size()-1).getWinnerMove();
-            Move counterMove = Move.Rock;
-            if (recent.get(recent.size()-1).getWinnerMove() == Move.Rock)
-            {
-                counterMove = Move.Paper;
-            }
-            if (recent.get(recent.size()-1).getWinnerMove() == Move.Paper)
-            {
-                counterMove = Move.Scissor;
-            }
-            if (recent.get(recent.size()-1).getWinnerMove() == Move.Scissor)
-            {
-                counterMove = Move.Rock;
-            }
-            Move surrenderMove = Move.Rock;
-            if (recent.get(recent.size()-1).getWinnerMove() == Move.Rock)
-            {
-                surrenderMove = Move.Scissor;
-            }
-            if (recent.get(recent.size()-1).getWinnerMove() == Move.Paper)
-            {
-                surrenderMove = Move.Rock;
-            }
-            if (recent.get(recent.size()-1).getWinnerMove() == Move.Scissor)
-            {
-                surrenderMove = Move.Paper;
-            }
-
-            return repeatLast > counterLast ? repeatMove : counterLast > surrenderLast ? counterMove : surrenderMove;
+            return Move.Scissor;
         }
+        System.out.println(repeatLast + " " + counterLast + " " + surrenderLast);
+
+        Move repeatMove = recent.get(recent.size()-1).getWinnerMove();
+
+        Move counterMove = Move.Rock;
+        if (recent.get(recent.size()-1).getWinnerMove() == Move.Rock)
+        {
+            counterMove = Move.Paper;
+        }
+        if (recent.get(recent.size()-1).getWinnerMove() == Move.Paper)
+        {
+            counterMove = Move.Scissor;
+        }
+        if (recent.get(recent.size()-1).getWinnerMove() == Move.Scissor)
+        {
+            counterMove = Move.Rock;
+        }
+
+        Move surrenderMove = Move.Rock;
+        if (recent.get(recent.size()-1).getWinnerMove() == Move.Rock)
+        {
+            surrenderMove = Move.Scissor;
+        }
+        if (recent.get(recent.size()-1).getWinnerMove() == Move.Paper)
+        {
+            surrenderMove = Move.Rock;
+        }
+        if (recent.get(recent.size()-1).getWinnerMove() == Move.Scissor)
+        {
+            surrenderMove = Move.Paper;
+        }
+
+        return repeatLast > counterLast && repeatLast > surrenderLast ? repeatMove : counterLast > surrenderLast ? counterMove : surrenderMove ;
     }
 
     //HELPER CLASSES
